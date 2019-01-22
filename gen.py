945b31a5a28ea75bd7bc063c02f9742149e09fd4
Sravanidhadi <sravanidhadi09@gmail.com>
2019-01-22 10:45:40 +0530

    done

---
+++ gen.py
@@ -0,0 +1,17 @@
+def my_gen():
+	n=1
+	print("This is printed first")
+	yield n
+	
+	n += 1
+	print("This is printed second")
+	yield n
+
+	n =+ 1
+	print("This is printed at last")
+	yield n
+a =my_gen()
+next(a)
+next(a)
+next(a)
+next(a)
